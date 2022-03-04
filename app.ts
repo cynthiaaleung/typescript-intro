f00d7a302ef41c0ef1cfc66d001ecb0bfd0f5928
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-03-04 14:24:33 -0800

    notes on function types and unknown, never type

--- app.ts
+++ app.ts
@@ -1,26 +1,16 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Cynthia';
+
+if (typeof userInput === 'string'){
+  userName = userInput; 
 }
 
-// return type is void, means function does not return anything, don't have to explicitly assign void
-function printResult(num: number): void {
-  console.log('Result: ' + num)
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+  // while(true) {} infinite loops also never return anything
 }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-// function types: combineValues takes in a function with 2 parameters, each being a number, returns a number
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
+generateError('An error occurred', 500);
