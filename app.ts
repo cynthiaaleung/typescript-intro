883f31daf780c27330d459915b0e240414d94b65
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-03-04 14:14:30 -0800

    add notes on union-aliases and function types

--- app.ts
+++ app.ts
@@ -1,25 +1,26 @@
-type Combinable = number | string; // this is an type alias
-type ConversionDescriptor = 'as-number' | 'as-text'
-
-function combine(
-  input1: Combinable, 
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+// return type is void, means function does not return anything, don't have to explicitly assign void
+function printResult(num: number): void {
+  console.log('Result: ' + num)
+}
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+printResult(add(5, 12));
+
+// function types: combineValues takes in a function with 2 parameters, each being a number, returns a number
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
